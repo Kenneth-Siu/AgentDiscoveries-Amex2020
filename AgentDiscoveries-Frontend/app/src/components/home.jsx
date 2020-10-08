c6ed33a89843e35bc57b9b706a2422776be80192
jd-hilton <j+work@hiltons.org.uk>
2020-10-08 16:29:47 +0100

    Landing page contains time zones

--- AgentDiscoveries-Frontend/app/src/components/home.jsx
+++ AgentDiscoveries-Frontend/app/src/components/home.jsx
@@ -1,11 +1,42 @@
-import * as React from 'react';
+import React from 'react';
+import {apiGet} from './utilities/request-helper';
 
-export default class Home extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello P.A.T.R.I.O.T. agents</h1>
-            </div>
+const Home = () => (
+    <div>
+        <Welcome/>
+        <TimeZones/>
+    </div>
+);
+
+const Welcome = () => (
+    <div>
+        <h1>Hello P.A.T.R.I.O.T. agents</h1>
+    </div>
+);
+
+const TimeZones = () => {
+    const [timeZones, setTimeZones] = React.useState({});
+
+    React.useEffect(() => {
+        apiGet('locations').then(locations => setTimeZones(locations.reduce((timeZones, location) => {
+            timeZones[location.timeZone] = true;
+            return timeZones;
+        }, {}))
         );
-    }
-}
+    }, []);
+
+    return (
+        <div>
+            {Object.keys(timeZones).map((timeZone, index) => <TimeZone key={index} timeZone={timeZone}/>) }
+        </div>
+    );
+};
+
+const TimeZone = ({timeZone}) => (
+    <div>
+        <span><strong>Time Zone:</strong> {timeZone}</span>
+        <span><strong>Time:</strong> {new Date().toLocaleString('en-US', {timeZone})}</span>
+    </div>
+);
+
+export default Home;
